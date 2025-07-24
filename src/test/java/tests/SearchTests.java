029f8ca60267be94c3a7ccb597882b3e4898a35f
Dinar <19yakudza99@gmail.com>
2025-07-24 18:32:45 +0500

    homework was done

---
+++ src/test/java/tests/SearchTests.java
@@ -0,0 +1,37 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import pages.MainPage;
+import pages.SearchPage;
+
+import static io.qameta.allure.Allure.step;
+
+public class SearchTests extends TestBase {
+
+    MainPage mainPage = new MainPage();
+    SearchPage searchPage = new SearchPage();
+
+    @Test
+    void successfulSearchTest() {
+        step("Open search and type query", () -> {
+            mainPage.clickSearchIcon();
+            searchPage.enterSearchQuery("Appium");
+        });
+
+        step("Verify content found", searchPage::shouldSeeResults);
+    }
+
+    @Test
+    void openAnyArticleTest() {
+        step("Open search and type query", () -> {
+            mainPage.clickSearchIcon();
+            searchPage.enterSearchQuery("Allure");
+        });
+
+        step("Open first article", searchPage::openFirstSearchResult);
+
+        step("Verify article page is opened", () -> {
+            searchPage.shouldSeeArticleTitle("Allure");
+        });
+    }
+}
